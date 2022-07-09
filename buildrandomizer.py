36b7fca9eda39158c1c3bac914f1d35fa43d6d91
Die4Ever <die4ever2005@gmail.com>
2022-07-08 22:57:55 -0500

    GitHub Actions execute binary

--- buildrandomizer.py
+++ buildrandomizer.py
@@ -1,4 +1,18 @@
 from BuildLibs.grp import *
 import BuildLibs.gui
+import argparse
+
+parser = argparse.ArgumentParser(description='Build Engine Randomizer')
+
+parser.add_argument('--version', action="store_true", help='Output version')
+parser.add_argument('--verbose', action="store_true", help="Output way more to the screen")
+args = parser.parse_args()
+
+if args.verbose:
+    setVerbose(2)
+
+if args.version:
+    print('0.0')
+    sys.exit(0)
 
 BuildLibs.gui.main()
