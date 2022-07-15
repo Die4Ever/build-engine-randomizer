0358ae8fc3c31e13f9842f05f31c4a1afc1a40d5
Die4Ever <die4ever2005@gmail.com>
2022-07-14 23:15:57 -0500

    v0.5.1 bump version number

--- BuildLibs/__init__.py
+++ BuildLibs/__init__.py
@@ -13,7 +13,7 @@ import random
 import pathlib
 
 def GetVersion() -> str:
-    return 'v0.5 Alpha'
+    return 'v0.5.1 Alpha'
 
 class FancyPacker:
     def __init__(self, endianness: str, mappings: dict):
