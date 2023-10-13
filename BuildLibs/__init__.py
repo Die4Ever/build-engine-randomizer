7b2ae92d18d114777e729ddb9d19a286a1117264
Die4Ever <die4ever2005@gmail.com>
2023-10-13 13:26:49 -0500

    v0.9.5 Beta bumped version number, release candidate

--- BuildLibs/__init__.py
+++ BuildLibs/__init__.py
@@ -16,7 +16,7 @@ from datetime import datetime
 import re
 
 def GetVersion() -> str:
-    return 'v0.9.4 Beta'
+    return 'v0.9.5 Beta'
 
 packLengthRegex = re.compile('^(.*?)(\d+)(\w)(.*?)$')
 class FancyPacker:
